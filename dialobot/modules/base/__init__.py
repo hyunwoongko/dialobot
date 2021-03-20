af370b4787b671f03750492040040fb8feb96853
hyunwoongko <gusdnd852@naver.com>
2021-03-21 08:32:18 +0900

    Add IntentRetriever

---
+++ dialobot/modules/base/__init__.py
@@ -0,0 +1,16 @@
+from dialobot.modules.base.intent_base import IntentBase
+
+__all__ = [IntentBase]
+# Copyright (c) 2021, Dialobot. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
