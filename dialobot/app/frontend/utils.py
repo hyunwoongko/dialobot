da12bb9938b2b5be70f5b1973185209424b9aa98
hyunwoongko <gusdnd852@naver.com>
2021-05-19 22:59:37 +0900

    modify running method of web application

--- dialobot/app/frontend/utils.py
+++ dialobot/app/frontend/utils.py
@@ -1,12 +1,12 @@
 import streamlit as st
+from dialobot.app.frontend.static.css import style
 
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(
-            f'<style>{f.read()}</style>',
-            unsafe_allow_html=True,
-        )
+def css():
+    st.markdown(
+        f'<style>{style}</style>',
+        unsafe_allow_html=True,
+    )
 
 
 def remote_css(url):
---
+++ dialobot/app/server.py
@@ -0,0 +1,6 @@
+from dialobot.app.backend import Backend
+from dialobot.app.frontend import Frontend
+
+if __name__ == '__main__':
+    Frontend()
+    Backend()
