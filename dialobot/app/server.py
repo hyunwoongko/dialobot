63590f2879a1f8b2e85c107225c21a174e5f770b
hyunwoongko <gusdnd852@naver.com>
2021-05-20 02:50:43 +0900

    modify import command

--- dialobot/app/server.py
+++ dialobot/app/server.py
@@ -1,12 +1,11 @@
-import streamlit as st
-from dialobot.app.frontend.pages import loading
-
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument("--backend_port", type=str, default=None)
     args = parser.parse_args()
 
+    import streamlit as st
+    from dialobot.app.frontend.pages import loading
     if not hasattr(st, "already_started_server"):
         st.already_started_server = True
         loading.page()
